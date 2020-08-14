c44ad4b2e3d3c232049bddbda241dd12a31be9cf
Didier VANDERSTOKEN <E_DVANDERSTOKEN@cafat.nc>
2020-08-14 15:25:41 +1100

    WIP: gateway with docker.

--- spring-cloud-gateway/src/main/java/nc/dva/gateway/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/nc/dva/gateway/SpringCloudGatewayApplication.java
@@ -2,10 +2,10 @@ package nc.dva.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class SpringCloudGatewayApplication {
 
     public static void main(String[] args) {
