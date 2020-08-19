db40f27217bf96f745d047346798048156d4b838
Didier VANDERSTOKEN <E_DVANDERSTOKEN@cafat.nc>
2020-08-19 16:20:46 +1100

    WIP

--- spring-cloud-gateway/src/main/java/nc/dva/gateway/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/nc/dva/gateway/SpringCloudGatewayApplication.java
@@ -1,15 +1,34 @@
 package nc.dva.gateway;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class SpringCloudGatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringCloudGatewayApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayApplication.class, args);
+	}
+
+	/**
+	 * Default Resilience4j circuit breaker configuration
+	 */
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3)).build()).build());
+	}
 
 }
--- spring-ms-client-backend/src/main/java/nc/dva/examples/ClientApplication.java
+++ spring-ms-client-backend/src/main/java/nc/dva/examples/ClientApplication.java
@@ -2,10 +2,8 @@ package nc.dva.examples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ClientApplication {
 
 	public static void main(String[] args) {
