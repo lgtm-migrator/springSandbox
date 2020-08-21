b3bc2b1ebed271fe2e247023acb42a03ae37b764
Didier VANDERSTOKEN <E_DVANDERSTOKEN@cafat.nc>
2020-08-21 16:42:06 +1100

    WIP

--- spring-ms-client-backend/src/main/java/nc/dva/examples/player/PlayerController.java
+++ spring-ms-client-backend/src/main/java/nc/dva/examples/player/PlayerController.java
@@ -6,12 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import nc.dva.examples.hystrix.Country;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class PlayerController {
+
+	private final String gatewayURI = "http://spring-cloud-gateway:10200";
+
+	private final RestTemplate restTemplate = new RestTemplate();
 
 	@Autowired
 	private PlayerRepository lRepository;
@@ -33,6 +41,21 @@ public class PlayerController {
 		}
 
 		return response;
+	}
+
+	@GetMapping(path = "/api/v1/countries")
+	public HttpEntity<?> getAllCountries() {
+
+		String finalURI = gatewayURI + "/geolocation/api/v1/countries";
+
+		System.out.println(finalURI);
+
+		Country[] result = restTemplate.getForObject(finalURI, Country[].class);
+
+		System.out.println(result);
+
+		return new HttpEntity<Object>(result);
+
 	}
 
 }
