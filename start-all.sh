de8e6d4f40691b7ae63119141eb10311eab36a7d
Didier VANDERSTOKEN <E_DVANDERSTOKEN@cafat.nc>
2020-08-27 08:39:33 +1100

    Shell startup correctness (configurations)

--- start-all.sh
+++ start-all.sh
@@ -8,8 +8,8 @@ java -Xms128m -Xmx192m -jar -Dspring.profiles.active=local ./spring-cloud-config
 sleep 10
 java -Xms128m -Xmx192m -jar -Dspring.profiles.active=local ./spring-cloud-gateway/target/spring-cloud-gateway-20.08.jar &
 sleep 10
-java -Xms128m -Xmx192m -jar -Dspring.profiles.active=local,peer1 ./spring-ms-countries/target/spring-ms-countries-20.08.jar &
+java -Xms128m -Xmx192m -jar -Dspring.profiles.active=local,peer1 ./spring-ms-countries/target/spring-ms-countries-20.08.jar --spring.config.location=classpath:/application-local.yml,classpath:/application-peer1.yml &
 sleep 10
-java -Xms128m -Xmx192m -jar -Dspring.profiles.active=local,peer2 ./spring-ms-countries/target/spring-ms-countries-20.08.jar &
+java -Xms128m -Xmx192m -jar -Dspring.profiles.active=local,peer2 ./spring-ms-countries/target/spring-ms-countries-20.08.jar --spring.config.location=classpath:/application-local.yml,classpath:/application-peer2.yml &
 sleep 10
 java -Xms128m -Xmx192m -jar -Dspring.profiles.active=local ./spring-ms-client-backend/target/spring-ms-client-backend-20.08.jar &
